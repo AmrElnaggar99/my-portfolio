a4e22591e3347c6a08f088acec3aa10deeb8d19a
Amr Elnaggar <amr.eln2gar@gmail.com>
2025-02-25 07:10:06 +0200

    fix stickynav default color

--- components/StickyHeader.tsx
+++ components/StickyHeader.tsx
@@ -98,7 +98,7 @@ function StickyHeader({ active }: { active: string }) {
                 key={item.title}
                 onClick={() => scrollTo(item.slide)}
                 className={`${
-                  active === item.slide ? "bg-white text-black" : ""
+                  active === item.slide ? "bg-white text-black" : "text-white"
                 } rounded-full cursor-pointer md:px-6 transition duration-300 lg:px-12 h-fit py-2 flex items-center w-fit`}
               >
                 {item.title}
