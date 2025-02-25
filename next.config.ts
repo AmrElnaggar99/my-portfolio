a5dcc9d582bcc6343078107b3b217a7e2a83d4e1
Amr Elnaggar <amr.eln2gar@gmail.com>
2025-02-25 17:11:38 +0200

    update next config to images.remotePatterns

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,13 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   images: {
-    domains: ["i.scdn.co"], // Allow Spotify images
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "i.scdn.co",
+        pathname: "**",
+      },
+    ],
   },
 };
 
