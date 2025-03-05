020ec1413786a2979024f14b98f54e15b581773c
Amr Elnaggar <amr.eln2gar@gmail.com>
2025-03-05 04:58:29 +0200

    remove animations from spotifyslide

--- components/slides/SpotifySlide.tsx
+++ components/slides/SpotifySlide.tsx
@@ -16,29 +16,21 @@ function SpotifySlide({ artists }: { artists: ArtistProps[] }) {
       <div className="p-6 font-monasans items-center text-center">
         <div className="relative z-0 flex items-center flex-col">
           <div className="mb-3">
-            <AnimatedHead className="text-gradient bg-gradient-to-r from-[#EEA525] to-[#FADC3A] bg-clip-text text-transparent">
+            <div className="text-gradient bg-gradient-to-r from-[#EEA525] to-[#FADC3A] bg-clip-text text-transparent">
               ✨ Bonus section ✨
-            </AnimatedHead>
+            </div>
           </div>
-          <h2 className="text-[clamp(2.5rem,8vw,3.5rem)] font-bold mb-4">
-            <AnimatedHead className="text-white">My Most Played Artists</AnimatedHead>
+          <h2 className="text-[clamp(2.5rem,8vw,3.5rem)] text-white font-bold mb-4">
+            My Most Played Artists
           </h2>
           <p className="text-gray-300">
-            <TypingText>
-              Music fuels my creativity. Here's what I've been listening to, fetched live from
-              Spotify
-            </TypingText>
+            Music fuels my creativity. Here's what I've been listening to, fetched live from Spotify
           </p>
         </div>
       </div>
-      <motion.div
-        initial={{ opacity: 0 }}
-        animate={{ opacity: 1 }}
-        transition={{ duration: 1.5 }}
-        className="relative w-full py-8 px-4 md:px-8 overflow-hidden"
-      >
+      <div className="relative w-full py-8 px-4 md:px-8 overflow-hidden">
         <ArtistCardCloud data={artists} />
-      </motion.div>
+      </div>
     </motion.div>
   );
 }
