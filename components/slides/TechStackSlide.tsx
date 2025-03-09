e2df54e69ad5ca8c5d3b3352dfd4d1958c5543af
Amr Elnaggar <amr.eln2gar@gmail.com>
2025-03-09 07:45:04 +0200

    implement lavalamp on filters

--- components/slides/TechStackSlide.tsx
+++ components/slides/TechStackSlide.tsx
@@ -1,11 +1,12 @@
 "use client";
-import React, { useEffect, useState } from "react";
+import React, { useCallback, useEffect, useLayoutEffect, useRef, useState } from "react";
 import AnimatedHead from "@/components/animations/AnimatedHead";
 import Slide from "./Slide";
 import SlidingText from "@/components/animations/SlidingText";
 import BubbleCloud from "@/components/BubbleCloud";
 import { FiSearch } from "react-icons/fi";
 import MyTechStackArray from "@/utils/MyTechStackArray";
+import { motion } from "framer-motion";
 
 function TechStackSlide({
   setActive,
@@ -30,7 +31,6 @@ function TechStackSlide({
         item.text.toLowerCase().includes(searchQuery.toLowerCase()) &&
         (selectedFilter.toLowerCase() === "all" || item.category === selectedFilter.toLowerCase()),
     );
-
     setFilteredItems(filtered);
   }, [searchQuery, selectedFilter]);
 
@@ -53,30 +53,15 @@ function TechStackSlide({
           </div>
         </h2>
       </div>
-      <div className="mt-5 flex max-w-[800px] mx-auto items-center justify-center gap-6 flex-wrap p-3">
-        <div className="flex min-h-10 flex-wrap gap-2">
-          {["All", "Frontend", "Backend", "Database", "Others"].map((filter) => (
-            <button
-              key={filter}
-              onClick={() => handleFilterChange(filter)}
-              className={`px-4 py-2 rounded-full text-sm font-medium transition duration-300
-            ${
-              selectedFilter.toLowerCase() === filter.toLowerCase()
-                ? "bg-white text-black"
-                : "bg-transparent border border-white hover:bg-white hover:text-black text-gray-300"
-            }`}
-            >
-              {filter.charAt(0).toUpperCase() + filter.slice(1)}
-            </button>
-          ))}
-        </div>
-        <div className="relative my-2 flex-1 min-w-[300px] max-w-[427px]">
+      <div className="mt-5 flex max-w-[800px] mx-auto items-center justify-center gap-4 lg:gap-6 flex-wrap p-3">
+        <FilterButtons selectedFilter={selectedFilter} handleFilterChange={handleFilterChange} />
+        <div className="relative my-2 flex-1 min-w-[300px] max-w-[448.4px]">
           <input
             type="text"
             placeholder="Search technologies"
             value={searchQuery}
             onChange={handleSearchChange}
-            className="focus-visible:bg-white hover:bg-white hover:text-black transition duration-300 focus-visible:text-black pr-4 pl-12 py-2 rounded-full border border-white text-white bg-transparent focus:outline-none w-full"
+            className="relative flex flex-wrap gap-2 py-0 isolate w-full select-none bg-white/5 sm:rounded-full rounded-md h-[44px] focus-visible:bg-white/10 hover:bg-white/10 hover:text-white transition duration-300 focus-visible:text-white pr-4 pl-12 border-white text-white focus:outline-none"
           />
           <div className="absolute left-4 top-1/2 transform -translate-y-1/2">
             <FiSearch className="text-gray-400 text-xl" />
@@ -87,6 +72,92 @@ function TechStackSlide({
         <BubbleCloud data={filteredItems} />
       </div>
     </Slide>
+  );
+}
+
+const FilterButtons = ({
+  selectedFilter,
+  handleFilterChange,
+}: {
+  selectedFilter: string;
+  handleFilterChange: (filter: string) => void;
+}) => {
+  const [activeStyle, setActiveStyle] = useState({ left: 0, width: 0, opacity: 1, top: 0 });
+  const activeRef = useRef<HTMLDivElement>(null);
+  const containerRef = useRef<HTMLDivElement>(null);
+  const filters = ["All", "Frontend", "Backend", "Database", "Others"];
+
+  const updateActiveStyle = useCallback(() => {
+    requestAnimationFrame(() => {
+      const activeItem = document.querySelector(`[data-filter='${selectedFilter.toLowerCase()}']`);
+      if (activeItem && containerRef.current) {
+        const rect = activeItem.getBoundingClientRect();
+        const containerRect = containerRef.current.getBoundingClientRect();
+        setActiveStyle({
+          left: rect.left - containerRect.left,
+          top: rect.top - containerRect.top,
+          width: rect.width,
+          opacity: 1,
+        });
+      }
+    });
+  }, [selectedFilter]);
+
+  useLayoutEffect(() => {
+    updateActiveStyle();
+    window.addEventListener("resize", updateActiveStyle);
+    return () => window.removeEventListener("resize", updateActiveStyle);
+  }, [updateActiveStyle]);
+
+  return (
+    <div
+      ref={containerRef}
+      className="relative flex flex-wrap gap-2 p-1 isolate w-fit select-none bg-white/5 sm:rounded-full rounded-md min-h-[44px]"
+    >
+      <ActiveBackgroundEffect activeStyle={activeStyle} activeRef={activeRef} />
+
+      {filters.map((filter) => (
+        <button
+          key={filter}
+          data-filter={filter.toLowerCase()}
+          onClick={() => handleFilterChange(filter)}
+          className={`relative z-10 px-4 py-2 rounded-full text-sm font-medium transition duration-300 ${
+            selectedFilter.toLowerCase() === filter.toLowerCase()
+              ? "text-white font-semibold"
+              : "text-gray-300 hover:text-white"
+          }`}
+        >
+          {filter}
+        </button>
+      ))}
+    </div>
+  );
+};
+
+function ActiveBackgroundEffect({
+  activeRef,
+  activeStyle,
+}: {
+  activeRef: React.RefObject<HTMLDivElement | null>;
+  activeStyle: {
+    left: number;
+    width: number;
+    opacity: number;
+    top: number;
+  };
+}) {
+  return (
+    <motion.div
+      ref={activeRef}
+      className="absolute h-[36px] bg-gray-950 rounded-full flex items-center justify-center transition-colors duration-300 font-bold z-0"
+      initial={activeStyle}
+      animate={activeStyle}
+      transition={{
+        type: "spring",
+        stiffness: 300,
+        damping: 30,
+      }}
+    />
   );
 }
 
